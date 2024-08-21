583293da1d1c1f71d883fae04d925cde12a326d2
jaeheon jeong <huchehuche@naver.com>
2024-08-21 17:05:12 +0900

    first commit

---
+++ src/senicare.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './senicare.css';
+import Auth from 'src/views/Auth';
+
+export default function Senicare() {
+  return (
+    <>
+    <Auth />
+    </>
+  );
+}
+
