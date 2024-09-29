c1968403201e6724285c35805945bcc772b987d3
jaeheon jeong <huchehuche@naver.com>
2024-09-30 08:58:48 +0900

    240930

--- src/views/CS/UpDate/index.tsx
+++ src/views/CS/UpDate/index.tsx
@@ -8,10 +8,12 @@ import { useNavigate, useParams } from 'react-router';
 import { Address, useDaumPostcodePopup } from 'react-daum-postcode';
 import { GetNurseListResponseDto } from 'src/apis/dto/response/nurse';
 import { ResponseDto } from 'src/apis/dto/response';
-import { ACCESS_TOKEN, CS_ABSOLUTE_PATH } from 'src/constants';
-import { fileUploadRequest, getCustomerRequest, getNurseListRequest, postCustomerRequest } from 'src/apis';
+import { ACCESS_TOKEN, CS_ABSOLUTE_PATH, CS_DETAIL_ABSOLUTE_PATH } from 'src/constants';
+import { fileUploadRequest, getCustomerRequest, getNurseListRequest, patchCustomerRequest, postCustomerRequest } from 'src/apis';
 import PostCustomerRequestDto from 'src/apis/dto/request/customer/post-customer.request.dto';
 import Pagination from 'src/components/Pagination';
+import { GetCustomerResponseDto } from 'src/apis/dto/response/customer';
+import { PatchCustomerRequestDto } from 'src/apis/dto/request/customer';
 
 // variable: 기본 프로필 이미지 URL //
 const defaultProfileImageUrl = 'https://blog.kakaocdn.net/dn/4CElL/btrQw18lZMc/Q0oOxqQNdL6kZp0iSKLbV1/img.png';
@@ -70,6 +72,32 @@ export default function CSUpdate() {
         setLocation(sigungu);
     };
 
+    // function: get customer resposne 처리 함수 //
+    const getCustomerResponse = (responseBody: GetCustomerResponseDto | ResponseDto | null) => {
+        const message = 
+            !responseBody ? '서버에 문제가 있습니다.' : 
+            responseBody.code === 'VF' ? '잘못된 접근입니다.' :
+            responseBody.code === 'AF' ? '잘못된 접근입니다.' :
+            responseBody.code === 'NC' ? '존재하지 않는 고객입니다.' :
+            responseBody.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
+        
+        const isSuccessed = responseBody !== null && responseBody.code === 'SU';
+        if (!isSuccessed) {
+            alert(message);
+            navigator(CS_ABSOLUTE_PATH);
+            return;
+        }
+
+        const { profileImage, name, birth, chargerId, chargerName, address, location } = responseBody as GetCustomerResponseDto;
+        setPreviewUrl(profileImage);
+        setName(name);
+        setBirth(birth);
+        setCharger(chargerId);
+        setChargerName(chargerName);
+        setAddress(address);
+        setLocation(location);
+    };
+
     // function: get nurse list response 처리 함수 //
     const getNurseListResponse = (responseBody: GetNurseListResponseDto | ResponseDto | null) => {
         const message =
@@ -89,11 +117,12 @@ export default function CSUpdate() {
     };
 
     // function: post customer response 처리 함수 //
-    const postCustomerResponse = (responseBody: ResponseDto | null) => {
+    const patchCustomerResponse = (responseBody: ResponseDto | null) => {
         const message = 
             !responseBody ? '서버에 문제가 있습니다.' : 
             responseBody.code === 'VF' ? '모두 입력해주세요.' :
             responseBody.code === 'AF' ? '잘못된 접근입니다.' :
+            responseBody.code === 'NC' ? '존재하지 않는 고객입니다.' :
             responseBody.code === 'NI' ? '존재하지 않는 요양사입니다.' :
             responseBody.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
 
@@ -103,7 +132,8 @@ export default function CSUpdate() {
             return;
         }
 
-        navigator(CS_ABSOLUTE_PATH);
+        if (!customerNumber) return;
+        navigator(CS_DETAIL_ABSOLUTE_PATH(customerNumber));
     };
 
     // event handler: 프로필 이미지 클릭 이벤트 처리 //
@@ -191,8 +221,9 @@ export default function CSUpdate() {
         navigator(CS_ABSOLUTE_PATH);
     };
 
-    // event handler: 등록 버튼 클릭 이벤트 처리 //
-    const onPostClickHandler = async () => {
+    // event handler: 수정 버튼 클릭 이벤트 처리 //
+    const onUpdateClickHandler = async () => {
+        if (!customerNumber) return;
         if (!name || !birth || !charger || !address || !location) return;
 
         const accessToken = cookies[ACCESS_TOKEN];
@@ -204,13 +235,13 @@ export default function CSUpdate() {
             formData.append('file', profileImageFile);
             url = await fileUploadRequest(formData);
         }
-        url = url ? url : defaultProfileImageUrl;
+        url = url ? url : previewUrl;
 
-        const requestBody: PostCustomerRequestDto = {
+        const requestBody: PatchCustomerRequestDto = {
             profileImage: url,
             name, birth, charger, address, location
         };
-        postCustomerRequest(requestBody, accessToken).then(postCustomerResponse);
+        patchCustomerRequest(requestBody, customerNumber, accessToken).then(patchCustomerResponse);
 
     };
 
@@ -223,7 +254,7 @@ export default function CSUpdate() {
 
         const accessToken = cookies[ACCESS_TOKEN];
         if (!accessToken) return;
-        getCustomerRequest(customerNumber, accessToken);
+        getCustomerRequest(customerNumber, accessToken).then(getCustomerResponse);
         getNurseListRequest(accessToken).then(getNurseListResponse);
     }, [customerNumber]);
 
@@ -271,7 +302,7 @@ export default function CSUpdate() {
             </div>
             <div className='bottom'>
                 <div className='button primary' onClick={onListButtonClickHandler}>목록</div>
-                <div className='button second' onClick={onPostClickHandler}>등록</div>
+                <div className='button second' onClick={onUpdateClickHandler}>수정</div>
             </div>
             {modalOpen &&
             <div className='modal'>
