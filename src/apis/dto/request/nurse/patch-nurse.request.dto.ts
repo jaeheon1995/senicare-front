6719da8addc0af7095839b93894dc54cf26be661
jaeheon jeong <huchehuche@naver.com>
2024-10-01 12:36:35 +0900

    240730.09

---
+++ src/apis/dto/request/nurse/patch-nurse.request.dto.ts
@@ -0,0 +1,4 @@
+//  interface: patch curse request body dto //
+export default interface PatchNurseRequestDto {
+    name: string;
+}
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,6 @@
+import useHrDetailUpdateStore from './hr-detail-update.store';
 import useSignInUserStore from './sign-in-user.store';
 
 export {
-    useSignInUserStore
-}
+    useHrDetailUpdateStore, useSignInUserStore
+};
