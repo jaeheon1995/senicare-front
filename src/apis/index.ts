1662126ffbdaa6ce248c989a544bf394e8fe2c65
jaeheon jeong <huchehuche@naver.com>
2024-10-16 16:05:25 +0900

    123

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -10,7 +10,7 @@ import { GetChargedCustomerResponseDto, GetNurseListResponseDto, GetNurseRespons
 import { GetToolListResponseDto, GetToolResponseDto } from "./dto/response/tool";
 
 // variable: API URL 상수 //
-const SENICARE_API_DOMAIN = 'http://localhost:4000';
+const SENICARE_API_DOMAIN = process.env.REACT_APP_API_URL;
 
 const AUTH_MODULE_URL = `${SENICARE_API_DOMAIN}/api/v1/auth`;
 
